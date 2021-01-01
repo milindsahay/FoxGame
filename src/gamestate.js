d68d2080d4b5aeac9cde54037bdee2e863415522
milindsahay <milind.sahay123@gmail.com>
2021-01-02 00:01:05 +0530

    css + UI

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,10 +1,13 @@
 const gamestate = {
-    state : "INIT",
-    clock : 1,
-    tick(){
-        this.clock++;
-        console.log(this.clock);
-        return this.clock
-    }
-}
+  state: "INIT",
+  clock: 1,
+  tick() {
+    this.clock++;
+    console.log(this.clock);
+    return this.clock;
+  },
+  handleUserAction(icon) {
+    console.log(icon);
+  },
+};
 export default gamestate;
--- src/init.js
+++ src/init.js
@@ -1,11 +1,14 @@
-import game from './gamestate'
-const TICK_RATE = 3000;
+import game from "./gamestate";
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
 
 function init() {
+  console.log("Starting game....");
+  initButtons(game.handleUserAction);
   //Closure, persisted time over async fn call
   let nextTimeToTick = Date.now();
   async function nextAnimationFrame() {
-    const now = Date.now()
+    const now = Date.now();
     if (nextTimeToTick <= now) {
       game.tick();
       nextTimeToTick = now + TICK_RATE;
