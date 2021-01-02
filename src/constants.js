c823ca0605c9f79bc86e564fb7afc366a605ff0d
milindsahay <milind.sahay123@gmail.com>
2021-01-03 00:05:38 +0530

    button structuring

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const TICK_RATE = 1000;
+export const ICONS = ["fish", "poop", "weather"];
+export const RAIN_CHANCE = 0.5;
+export const SCENES = ["day", "rain"];
+export const DAY_LENGTH = 40;
+export const NIGHT_LENGTH = 3;
+export const getHungryTime = (clock) =>
+  clock + Math.floor(Math.random() * 3) + 5;
+export const getDieTime = (clock) => clock + Math.floor(Math.random() * 3) + 4;
+export const getPoopTime = (clock) => clock + Math.floor(Math.random() * 3) + 5;
