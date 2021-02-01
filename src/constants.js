8467fbffde1e608cf3994666d22308503f24ca83
Milind Sahay <milindsahay@192.168.1.7>
2021-02-02 00:06:54 +0530

    die time increase

--- src/constants.js
+++ src/constants.js
@@ -6,5 +6,5 @@ export const DAY_LENGTH = 40;
 export const NIGHT_LENGTH = 3;
 export const getHungryTime = (clock) =>
   clock + Math.floor(Math.random() * 3) + 5;
-export const getDieTime = (clock) => clock + Math.floor(Math.random() * 3) + 4;
+export const getDieTime = (clock) => clock + Math.floor(Math.random() * 3) + 8;
 export const getPoopTime = (clock) => clock + Math.floor(Math.random() * 3) + 5;
